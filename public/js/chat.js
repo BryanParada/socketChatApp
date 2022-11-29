a197e47d20fe75c29bffa99071bba71ea990927e
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-29 20:28:58 -0300

    N230 - Validate JWT - frontEnd

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,36 @@
+const url = ( window.location.hostname.includes('localhost'))
+? 'http://localhost:8080/api/auth/'
+: 'https://restserver-production-eb21.up.railway.app/api/auth/'
 
+let user   = null;
+let socket = null;
 
-const socket = io();
+// validar el token del localstorage
+const validate_JWT_LS = async() =>{
+    const token = localStorage.getItem('token') || '';
+
+    if( token.length <= 10){
+        window.location = 'index.html';
+        throw new Error('There is no token in the server');
+    }
+
+    const resp = await fetch( url, {
+        headers: {'x-token': token}
+    });
+
+    const { user: userDB, token: tokenDB} = await resp.json();
+    //console.log(userDB, tokenDB);
+    localStorage.setItem('token', tokenDB);
+    user = userDB;
+    
+}
+
+const main = async() => {
+
+    await validate_JWT_LS();
+
+} 
+
+main();
+
+//const socket = io();
