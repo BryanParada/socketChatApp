8f36374f2448b18fbcae3631263c1d46dfb94006
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-30 10:33:41 -0300

    N233 - model for controlling connected users and messages

--- models/index.js
+++ models/index.js
@@ -1,17 +1,19 @@
 
 
 const Category = require('./category')
+const ChatMessages = require('./chat-messages')
+const Product = require('./product')
 const Role = require('./role')
 const Server = require('./server')
 const User = require('./user')
-const Product = require('./product')
 
 
 module.exports = {
     Category,
+    ChatMessages,
+    Product,
     Role,
     Server,
     User,
-    Product
 
 }
